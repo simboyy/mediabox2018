7bab8d7dad1ba18e23f88cc34242a99f135110a5
simboyy <smkorera@gmail.com>
2018-01-23 16:23:07 +0200

    Update campaign.controller.js

--- server/api/campaign/campaign.controller.js
+++ server/api/campaign/campaign.controller.js
@@ -49,6 +49,19 @@ var _inventory = require('../inventory/inventory.model');
 
 var _inventory2 = _interopRequireDefault(_inventory);
 
+var currentDate = require("current-date");
+
+var randomInt = require('random-int');
+
+const Invoice = require("../lib");
+
+const path = require('path');
+var fs      = require('fs');
+const ABSPATH = path.dirname(process.mainModule.filename); // Absolute path to our app directory
+var filePath = path.join(ABSPATH, '/api/campaign/my-invoice.html');
+var filePath2 = path.join(ABSPATH, '/api/campaign/my-quote.html');
+
+
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
@@ -71,12 +84,12 @@ function sendSMS(contact) {
     content: 'Hello from Mediabox!'
   }, function (err, message) {
     if (err) throw err;
-    ////console.log(message);
+    ////////////////////////////////console.log(message);
   });
 }
 
 function InventoryUpdate(res) {
-  ////console.log(res.req.body.items);
+  //console.log(res.req.body.items);
   var items = res.req.body.items;
   _lodash2.default.each(items, function (item) {
 
@@ -89,26 +102,264 @@ function InventoryUpdate(res) {
     var yyyy = cdate.getFullYear();
     var q = { $and: [{ 'pname': item.publisher }, { 'vname': item.name }, { 'year': yyyy }, { 'startDate': { $lt: startDate } }, { 'endDate': { $gte: endDate } }] };
 
-    //console.log(q);
+   // console.log(q);
     _inventory2.default.findOneAndUpdate(q, { $inc: { "available": -1 } }, { upsert: false, setDefaultsOnInsert: true, runValidators: true }).exec();
   });
 }
 
-function CampaignPlaced(res, statusCode) {
+function CampaignPlaced(req,res, statusCode,filePath,filePath2) {
+    
+   // console.log(req.body.items);
+    
+    let myInvoice = new Invoice({
+            config: {
+                template: __dirname + "/template/index2.html"
+              , tableRowBlock: __dirname + "/template/blocks/row2.html"
+            }
+          , data: {
+                currencyBalance: {
+                    main: 1
+                  , secondary: 3.67
+                }
+              , invoice: {
+                    number: {
+                        series: "2018"
+                      , separator: "-"
+                      , id: randomInt(10, 100)
+                    }
+                  , date: currentDate('date')
+                  , dueDate: currentDate('date')
+                  , explanation: "Thank you for your business!"
+                  , currency: {
+                        main: "$"
+                      , secondary: "$"
+                    }
+                }
+              , tasks:req.body.items
+            }
+          , seller: {
+                company: "Karmiens Enterprises T/A Mediabox Advertising."
+              , registrationNumber: "F05/XX/YYYY"
+              , taxId: "0200092224"
+              , address: {
+                    street: "Impala Road"
+                  , number: "8"
+                  , zip: ""
+                  , city: "Harare"
+                  , region: ""
+                  , country: "Zimbabwe"
+                }
+              , phone: "+263772 580 474"
+              , email: "info@mediabox.co.zw"
+              , website: "www.mediabox.co.zw"
+              , bank: {
+                    name: "CBZ Bank Kwame Nkurumah"
+                  , swift: "COBZZWHAXXX"
+                  , currency: "$"
+                  , iban: "6101"
+                }
+            }
+          , buyer: {
+                company:  req.user.company
+
+              , taxId: "00000000"
+              , address: {
+                    street: "The Street Name"
+                  , number: "00"
+                  , zip: "000000"
+                  , city: "Some City"
+                  , region: "Some Region"
+                  , country: "Nowhere"
+                }
+              , phone: req.user.phone
+              , email:  req.user.email
+              , website: req.user.website
+              , bank: {
+                    name: "Some Bank Name"
+                  , swift: "XXXXXX"
+                  , currency: "XXX"
+                  , iban: "..."
+                }
+            }
+        });
+
+   //console.log(myInvoice);
+
+        // Render invoice as HTML and PDF
+        myInvoice.toHtml(__dirname + "/my-quote.html", (err, data) => {
+            console.log("Saved HTML file");
+        }).toPdf(__dirname + "/my-quote.pdf", (err, data) => {
+            console.log("Saved pdf file");
+
+
+            var api_key = 'key-a2cb48ef184b3e02e563b2d3a521aa48';
+            var domain = 'mg.mediabox.co.zw';
+            var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
+
+            var dataimage = fs.readFileSync(filePath);
+
+            var filename = 'my-quote.html';
+            var attch = new mailgun.Attachment({ dataimage, filename, contentType: 'application/pdf' });
+
+            var file = fs.readFileSync(filePath);
+             
+            var data = {
+               from: 'Mediabox Advertising<billing@mediabox.co.zw>',
+              to: 'smkorera@gmail.com',
+              subject: 'Advertising Quote',
+              text: 'Thank you for booking your advert using Mediabox!',
+              html:"<html><p>Dear "+req.user.name+" </p><p>This is a  notice that your quotation which was generated on&nbsp;<span class='aBn' tabindex='0' data-term='goog_1714329927'><span class='aQJ'>"+currentDate('date')+"</span></span>&nbsp;is now available.</p><p>You can login to your client area to view more details at&nbsp;<a href='https://advertising.mediabox.co.zw/campaign' target='_blank' >https://advertising.mediabox.co.zw/campaign</a></p><p>Mediabox Advertising  (PVT) LTD</p></html>",
+              attachment:filePath2
+            };
+             
+            mailgun.messages().send(data, function (error, body) {
+              console.log(body);
+            });
+
+
+        });
+
+
+
+
   res.req.body.to = res.req.body.email;
-  email.send(config.mailOptions.CampaignPlaced(res.req.body));
+  email.send(config.mailOptions.CampaignPlaced(res.req.body,filePath));
 
   statusCode = statusCode || 200;
   return function (entity) {
     if (entity) {
       res.status(statusCode).json(entity);
     }
-  };
+  };      
+
 }
+
+
+function genereteQuote(req,res){
+  console.log(req.body.items);
+
+   let myInvoice = new Invoice({
+            config: {
+                template: __dirname + "/template/index2.html"
+              , tableRowBlock: __dirname + "/template/blocks/row2.html"
+            }
+          , data: {
+                currencyBalance: {
+                    main: 1
+                  , secondary: 3.67
+                }
+              , invoice: {
+                    number: {
+                        series: "2018"
+                      , separator: "-"
+                      , id: randomInt(10, 100)
+                    }
+                  , date: currentDate('date')
+                  , dueDate: currentDate('date')
+                  , explanation: "Thank you for your business!"
+                  , currency: {
+                        main: "$"
+                      , secondary: "$"
+                    }
+                }
+              , tasks:req.req.body.items
+            }
+          , seller: {
+                company: "Karmiens Enterprises T/A Mediabox Advertising."
+              , registrationNumber: "F05/XX/YYYY"
+              , taxId: "0200092224"
+              , address: {
+                    street: "Impala Road"
+                  , number: "8"
+                  , zip: ""
+                  , city: "Harare"
+                  , region: ""
+                  , country: "Zimbabwe"
+                }
+              , phone: "+263772 580 474"
+              , email: "info@mediabox.co.zw"
+              , website: "www.mediabox.co.zw"
+              , bank: {
+                    name: "CBZ Bank Kwame Nkurumah"
+                  , swift: "COBZZWHAXXX"
+                  , currency: "$"
+                  , iban: "6101"
+                }
+            }
+          , buyer: {
+                company:  req.user.company
+
+              , taxId: "00000000"
+              , address: {
+                    street: "The Street Name"
+                  , number: "00"
+                  , zip: "000000"
+                  , city: "Some City"
+                  , region: "Some Region"
+                  , country: "Nowhere"
+                }
+              , phone: req.user.phone
+              , email:  req.user.email
+              , website: req.user.website
+              , bank: {
+                    name: "Some Bank Name"
+                  , swift: "XXXXXX"
+                  , currency: "XXX"
+                  , iban: "..."
+                }
+            }
+        });
+
+   //console.log(myInvoice);
+
+        // Render invoice as HTML and PDF
+        myInvoice.toHtml(__dirname + "/my-quote.html", (err, data) => {
+            console.log("Saved HTML file");
+        }).toPdf(__dirname + "/my-quote.pdf", (err, data) => {
+            console.log("Saved pdf file");
+
+
+            var api_key = 'key-a2cb48ef184b3e02e563b2d3a521aa48';
+            var domain = 'mg.mediabox.co.zw';
+            var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
+
+            var dataimage = fs.readFileSync(filePath);
+
+            var filename = 'my-invoice.html';
+            var attch = new mailgun.Attachment({ dataimage, filename, contentType: 'application/pdf' });
+
+            var file = fs.readFileSync(filePath);
+             
+            var data = {
+              from: 'Mediabox Advertising<billing@mediabox.co.zw>',
+              to: 'smkorera@gmail.com',
+              subject: 'Advertising Quote',
+              text: 'Thank you for booking your advert using Mediabox!',
+              html:"<html><p>Dear "+updated.items[0].advertiser.name+" </p><p>This is a  notice that your quotation which was generated on&nbsp;<span class='aBn' tabindex='0' data-term='goog_1714329927'><span class='aQJ'>"+currentDate('date')+"</span></span>&nbsp;is now available.</p><p>You can login to your client area to view more details at&nbsp;<a href='https://advertising.mediabox.co.zw/campaign' target='_blank' >https://advertising.mediabox.co.zw/campaign</a></p><p>Mediabox Advertising  (PVT) LTD</p></html>",
+              attachment:filePath2
+            };
+             
+            mailgun.messages().send(data, function (error, body) {
+              console.log(body);
+            });
+
+
+        });
+
+        // Serve the pdf via streams (no files)
+        //require("http").createServer((req, res) => {
+           // myInvoice.toPdf({ output: res });
+        //}).listen(8000);
+        }
 
 function CampaignUpdated(res, statusCode) {
   email.send(config.mailOptions.CampaignUpdated(res.req.body));
 
+  //genereteInvoice(res,res);
+
+ 
   statusCode = statusCode || 200;
   return function (entity) {
     if (entity) {
@@ -128,12 +379,143 @@ function respondWithResult(res, statusCode) {
 
 function saveUpdates(updates) {
 
-  ////console.log(updates);
-
+  
 
   return function (entity) {
 
     var updated = _lodash2.default.merge(entity, updates);
+    var itemsArray = [];    
+    var total = 0;
+    
+    itemsArray = updated.items;
+
+    for (var i = 0; i < itemsArray.length; i++) {
+      total = parseInt(itemsArray[i].price )* parseInt(itemsArray[i].quantity);
+    }
+
+    
+    
+     total = total.toFixed(2);
+      if(updates.status!="Campaign Rejected"){
+      ///console.log(updated.items);  
+
+      var invoiceno = randomInt(10, 100);
+      
+
+
+
+      let myInvoice = new Invoice({
+            config: {
+                template: __dirname + "/template/index.html"
+              , tableRowBlock: __dirname + "/template/blocks/row.html"
+            }
+          , data: {
+                currencyBalance: {
+                    main: 1
+                  , secondary: 3.67
+                }
+              , invoice: {
+                    number: {
+                        series: "2018"
+                      , separator: "-"
+                      , id: invoiceno
+                    }
+                  , date: currentDate('date')
+                  , dueDate: currentDate('date')
+                  , explanation: "Thank you for your business!"
+                  , currency: {
+                        main: "$"
+                      , secondary: "$"
+                    }
+                }
+              , tasks:updated.items
+            }
+          , seller: {
+                company: "Karmiens Enterprises T/A Mediabox Advertising."
+              , registrationNumber: "F05/XX/YYYY"
+              , taxId: "0200092224"
+              , address: {
+                    street: "Impala Road"
+                  , number: "8"
+                  , zip: ""
+                  , city: "Harare"
+                  , region: ""
+                  , country: "Zimbabwe"
+                }
+              , phone: "+263772 580 474"
+              , email: "info@mediabox.co.zw"
+              , website: "www.mediabox.co.zw"
+              , bank: {
+                    name: "CBZ Bank Kwame Nkurumah"
+                  , swift: "COBZZWHAXXX"
+                  , currency: "$"
+                  , iban: "6101"
+                }
+            }
+          , buyer: {
+                company:  updated.items[0].advertiser.company
+
+              , taxId: "00000000"
+              , address: {
+                    street: "The Street Name"
+                  , number: "00"
+                  , zip: "000000"
+                  , city: "Some City"
+                  , region: "Some Region"
+                  , country: "Nowhere"
+                }
+              , phone: updated.items[0].advertiser.phone
+              , email:  updated.items[0].advertiser.email
+              , website: updated.items[0].advertiser.website
+              , bank: {
+                    name: "Some Bank Name"
+                  , swift: "XXXXXX"
+                  , currency: "XXX"
+                  , iban: "..."
+                }
+            }
+        });
+
+   //console.log(myInvoice);
+
+        // Render invoice as HTML and PDF
+        myInvoice.toHtml(__dirname + "/my-invoice.html", (err, data) => {
+            console.log("Saved HTML file");
+        }).toPdf(__dirname + "/my-invoice.pdf", (err, data) => {
+            console.log("Saved pdf file");
+
+
+            var api_key = 'key-a2cb48ef184b3e02e563b2d3a521aa48';
+            var domain = 'mg.mediabox.co.zw';
+            var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
+
+            var dataimage = fs.readFileSync(filePath);
+
+            var filename = 'my-invoice.html';
+            var attch = new mailgun.Attachment({ dataimage, filename, contentType: 'application/pdf' });
+
+            var file = fs.readFileSync(filePath);
+             
+            var data = {
+              from: 'Mediabox Advertising<billing@mediabox.co.zw>',
+              to: 'smkorera@gmail.com',
+              subject: 'Invoice Notice',
+              text: 'Thank you for booking your advert using Mediabox!',
+              html:"<html><p>Dear "+updated.items[0].advertiser.name+" </p><p>This is a billing notice that your invoice no. "+invoiceno+" which was generated on&nbsp;<span class='aBn' tabindex='0' data-term='goog_1714329927'><span class='aQJ'>"+currentDate('date')+"</span></span>&nbsp;is now available.</p><p>Your payment method is: Ecocash / Telecash / MasterCard / Visa / VPayments</p><p>Invoice: "+invoiceno+"<br />Balance Due: $"+total+" USD<br />Due Date:&nbsp;<span class='aBn' tabindex='0' data-term='goog_1714329928'><span class='aQJ'>"+currentDate('date')+"</span></span></p><p>You can login to your client area to view and pay the invoice at&nbsp;<a href='https://advertising.mediabox.co.zw/campaign' target='_blank' >https://advertising.mediabox.co.zw/campaign</a></p><p>Mediabox Advertising  (PVT) LTD</p></html>",
+              attachment:filePath
+            };
+             
+            mailgun.messages().send(data, function (error, body) {
+              console.log(body);
+            });
+
+
+        });
+
+      
+
+     }
 
     return updated.save().then(function (updated) {
       return updated;
@@ -164,6 +546,7 @@ function handleEntityNotFound(res) {
 function handleError(res, statusCode) {
   statusCode = statusCode || 500;
   return function (err) {
+
     res.status(statusCode).send(err);
   };
 }
@@ -179,9 +562,9 @@ function myCampaigns(req, res) {
     return str;
   }
   var q = isJson(req.query.where);
-  ////console.log(q);
+  //console.log(q);
 
-_campaign2.default.find(q).sort({created_at: -1}).exec(function (err, campaigns) {
+  _campaign2.default.find(q, function (err, campaigns) {
     if (err) {
       return handleError(res, err);
     }
@@ -193,14 +576,15 @@ _campaign2.default.find(q).sort({created_at: -1}).exec(function (err, campaigns)
 // List all advertising spaces
 
 function pubCampaignsCalendar(req, res) {
-  _order2.default.aggregate([{ $unwind: "$items" }, { $project: { _id: 0, title: "name", startsAt: "$items.startDate", endsAt: "$items.endDate", allDay: true, incrementsBadgeTotal: true, color: { primary: '#e3bc08', secondary: '#fdf1ba' } } }], function (err, result) {
-    if (err) {
-      //console.log(err);
-      return;
-    }
+  return 0;
+  // _order2.default.aggregate([{ $unwind: "$items" }, { $project: {  title: "name", start: "$items.startDate", end: "$items.endDate", allDay: false } }], function (err, result) {
+  //   if (err) {
+  //     console.log(err);
+  //     return;
+  //   }
 
-    return res.status(200).json(result);
-  });
+  //   return res.status(200).json(result);
+  // });
 }
 
 // Get all Campaigns by a publisher
@@ -214,9 +598,9 @@ function pubCampaigns(req, res) {
     return str;
   }
   var q = isJson(req.query.where);
-  ////console.log(q);
+  //console.log(q);
 
-  _campaign2.default.find(q).sort({created_at: -1}).exec(function (err, campaigns) {
+  _campaign2.default.find(q, function (err, campaigns) {
     if (err) {
       return handleError(res, err);
     }
@@ -226,7 +610,7 @@ function pubCampaigns(req, res) {
 
 // Gets a list of Campaigns
 function index(req, res) {
-  return _campaign2.default.find().sort({created_at: -1}).exec().then(respondWithResult(res)).catch(handleError(res));
+  return _campaign2.default.find().exec().then(respondWithResult(res)).catch(handleError(res));
 }
 
 // Gets a single Campaign from the DB
@@ -241,18 +625,19 @@ function create(req, res) {
   req.body.campaignNo = shortId.generate();
 
   // When Campaign.status is null, the client will replace with the Array[0] of Campaign status at Settings page
-  return _campaign2.default.create(req.body).then(CampaignPlaced(res, 201)).then(InventoryUpdate(res)).catch(handleError(res));
+  return _campaign2.default.create(req.body).then(sendSMS(res)).then(InventoryUpdate(res)).then(CampaignPlaced(req,res, 201,filePath,filePath2)).catch(handleError(res));
 }
 
 // Updates an existing Campaign in the DB
 function update(req, res) {
+
   if (req.body._id) {
     delete req.body._id;
   }
   if (req.body.__v) {
     delete req.body.__v;
   }
-  ////console.log(req.body);
+  //console.log(req.body);
 
   if (!req.body.status) {
 
