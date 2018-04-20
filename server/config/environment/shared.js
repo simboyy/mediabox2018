2e4a55be90d9166bed0c93a25c47b5df77502d90
simboyy <smkorera@gmail.com>
2018-04-20 22:14:10 +0200

    Update shared.js

--- server/config/environment/shared.js
+++ server/config/environment/shared.js
@@ -12,7 +12,7 @@ exports = module.exports = { // Can not put all settings here as this needs a re
     signUpAdvertiser: function signUpAdvertiser(body) {
       return {
         from: 'admin@adspaces.co.zw',
-        to: body.to,
+        to: 'smkorera@gmail.com',
         subject: 'Welcome to Adspaces!!',
         html: '<p><b>Thank you for registering for Adspaces.</b> You will find that it’s a great way to discover media options, build advertising campaigns ,make bookings and send adverts directly to  publishers with no back and forth .<br> Increase your sales and boost your profits without any significant legwork by discovering cost effective advertising media options through Adspaces.<p>Here are a few resources from our getting started section that might help you out.<br/><ul><li>Step-by-Step Guide to Your First Adspaces Campaign https://www.youtube.com/watch?v=OifS1gdXafAFAQS</li><li> What publishers can I find on Adspaces? How do I benefit ? http://www.mediabox.co.zw/index.html#faq </li></ul></p><p>Again I want to welcome you to our community. I cant wait to hear about your experience with Adspaces<br/>Enjoy redefined Convenience <br></p><p>Simbarashe Mukorera</p><p>P.S. I’m your customer support hero in charge of keeping you happy. If you have ANY questions... problems... or concerns... please feel free to reach out to ask me before getting frustrated (Phone:263 773 439 246 ,Skype: simbarashe.mukorera1, Email: admin@adspaces.co.zw)</p>' // html body
 
@@ -22,7 +22,7 @@ exports = module.exports = { // Can not put all settings here as this needs a re
     signUpPublisher: function signUpPublisher(body) {
       return {
         from: 'admin@adspaces.co.zw',
-        to: body.to,
+        to: 'smkorera@gmail.com',
         subject: 'Welcome to Adspaces!!',
         html: '<p><b>Thank you for registering for Adspaces.</b> You will find that it’s a great way to list your media options for free , connect with advertisers from around the globe   and  receive bookings and creative directly  from  advertisers.<br> Increase your sales and boost your profits by <b>ACCESSING GLOBAL DEMAND!</b> through Adspaces.<p>Again I want to welcome you to our community. I cant wait to hear about your experience with Adspaces<br/>Enjoy redefined Convenience <br><p/><p>Simbarashe Mukorera</p><p>P.S. I’m your customer support hero in charge of keeping you happy. If you have ANY questions... problems... or concerns... please feel free to reach out to ask me before getting frustrated (Skype: simbarashe.mukorera1, Email: admin@adspaces.co.zw)</p>' // html body
 
@@ -31,7 +31,7 @@ exports = module.exports = { // Can not put all settings here as this needs a re
     forgotPassword: function forgotPassword(body) {
       return {
         from: 'passwordreset@adspaces.co.zw',
-        to: body.to,
+        to: 'smkorera@gmail.com',
         subject: 'Adspaces Password Reset Request',
         text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' + 'Please click on the following link, or paste this into your browser to complete the process:\n\n' + 'http://' + body.host + '/reset/' + body.token + '\n\n' + 'If you did not request this, please ignore this email and your password will remain unchanged.\n'
       };
@@ -39,7 +39,7 @@ exports = module.exports = { // Can not put all settings here as this needs a re
     resetPassword: function resetPassword(body) {
       return {
         from: 'passwordreset@adspaces.co.zw',
-        to: body.to,
+       to: 'smkorera@gmail.com',
         subject: 'Adspaces Password Changed',
         text: 'Hello,\n\n' + 'This is a confirmation that the password for your account ' + body.to + ' has just been changed.\n'
       };
@@ -47,7 +47,7 @@ exports = module.exports = { // Can not put all settings here as this needs a re
     orderPlaced: function orderPlaced(body) {
       return {
         from: 'Adspaces <admin@adspaces.co.zw>',
-        to: body.to,
+        to: 'smkorera@gmail.com',
         subject: 'Order Placed Successfully',
         text: "Order No: " + body.orderNo + "\n Status: " + body.status + "\n\n Payment Method: " + body.payment_method + "\n\n Payment ID: " + body.id + "\n Amount: " + body.amount.total + " " + body.amount.currency + "\n\n Address: \n Name: " + body.address.recipient_name + "\n Line: " + body.address.line1 + "\n City: " + body.address.city + "\n State: " + body.address.state + "\n Zip: " + body.address.postal_code
       };
@@ -55,7 +55,7 @@ exports = module.exports = { // Can not put all settings here as this needs a re
     orderUpdated: function orderUpdated(body) {
       return {
         from: 'Adspaces <admin@adspaces.co.zw>',
-        to: body.to,
+        to: 'smkorera@gmail.com',
         subject: 'Your Order Status Updated',
         text: "Order No: " + body.orderNo + "\n Status: " + body.status + "\n\n Payment Method: " + body.payment_method + "\n\n Payment ID: " + body.id + "\n Amount: " + body.amount.total + " " + body.amount.currency + "\n\n Address: \n Name: " + body.address.recipient_name + "\n Line: " + body.address.line1 + "\n City: " + body.address.city + "\n State: " + body.address.state + "\n Zip: " + body.address.postal_code + "\n "
       };
@@ -63,7 +63,7 @@ exports = module.exports = { // Can not put all settings here as this needs a re
     CampaignPlaced: function CampaignPlaced(body) {
       return {
         from: 'Adspaces <admin@adspaces.co.zw>',
-        to: body.to,
+        to: 'smkorera@gmail.com',
         subject: 'New Campaign',
         text: "Your campaign has been created successfully campaign No: " + body.campaignNo
 
@@ -88,7 +88,7 @@ exports = module.exports = { // Can not put all settings here as this needs a re
     CampaignUpdated: function CampaignUpdated(body) {
       return {
         from: 'Adspaces <admin@adspaces.co.zw>',
-        to: body.to,
+       to: 'smkorera@gmail.com',
         subject: 'Campaign Status Updated',
         text: "\nYou have recieved feedback from publisher for  campaign : " + body.campaignName + "" + "   View update on  Adspaces https://adspaces.co.zw/campaign"
 
