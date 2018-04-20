24693ccf6d15fdd5e26cd8de160b53e2a90ffd54
simboyy <smkorera@gmail.com>
2018-04-20 22:02:00 +0200

    Update send.js

--- server/api/sendmail/send.js
+++ server/api/sendmail/send.js
@@ -6,8 +6,10 @@ Object.defineProperty(exports, "__esModule", {
 exports.send = send;
 var nodemailer = require('nodemailer');
 var sgTransport = require('nodemailer-sendgrid-transport');
+var mg = require('nodemailer-mailgun-transport');
 // html to text plugin 
 var htmlToText = require('nodemailer-html-to-text').htmlToText;
+var hbs       = require ('nodemailer-express-handlebars');
 
 function send(req) {
     var options = {
@@ -17,7 +19,21 @@ function send(req) {
     };
 
     var mailer = nodemailer.createTransport(sgTransport(options));
-    mailer.sendMail(req, function (error, info) {
+	
+	var auth = {
+	  auth: {
+		api_key: 'key-a2cb48ef184b3e02e563b2d3a521aa48',
+		domain: 'mg.mediabox.co.zw'
+	  },
+	  proxy: false // optional proxy, default is false
+	}
+
+var nodemailerMailgun = nodemailer.createTransport(mg(auth));
+	nodemailerMailgun.use('compile',hbs({
+		viewPath:'views/email',
+		extName:'.hbs'
+	}));
+    nodemailerMailgun.sendMail(req, function (error, info) {
 
         var res = {};
         if (error) {
